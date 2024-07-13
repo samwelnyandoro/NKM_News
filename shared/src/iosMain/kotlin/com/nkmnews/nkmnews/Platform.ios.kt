c8e1fe91272f5b77b74b974962ae271dbd9a8704
Samwel <samwelnyandoro01@gmail.com>
2024-07-13 22:26:25 +0300

    gradle

--- shared/src/iosMain/kotlin/com/nkmnews/nkmnews/Platform.ios.kt
+++ shared/src/iosMain/kotlin/com/nkmnews/nkmnews/Platform.ios.kt
@@ -5,5 +5,4 @@ import platform.UIKit.UIDevice
 class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
-
 actual fun getPlatform(): Platform = IOSPlatform()
